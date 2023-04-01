f2f87f6547fd868118a57078a4caa509ce8e9b8d
JoyceF-9 <118493335+JoyceF-9@users.noreply.github.com>
2023-04-01 11:37:36 -0400

    Added git repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Joyce Feng and is{" "}
+        <a
+          href="https://github.com/JoyceF-9/React-Weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
