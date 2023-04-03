9fcb7fb26ac251d159c09c8434df0dc83ac92287
JoyceF-9 <118493335+JoyceF-9@users.noreply.github.com>
2023-04-02 20:37:17 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Joyce Feng and is{" "}
-        <a
-          href="https://github.com/JoyceF-9/React-Weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Joyce Feng and is{" "}
+          <a
+            href="https://github.com/JoyceF-9/React-Weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
