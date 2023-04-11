0f1e86365fb84a950afafd3c277f24405ace519c
JoyceF-9 <118493335+JoyceF-9@users.noreply.github.com>
2023-04-10 21:30:31 -0400

    Weather Forecast updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
-      tempearture: response.data.main.temp,
-      description: response.data.weather[0].description,
-      date: new Date(response.data.dt * 1000),
-      icon: response.data.weather[0].icon,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
@@ -59,6 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
